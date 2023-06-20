d6f0b7832a4a9d6a8d255addceae41e5f5ce20bd
haikallf <haikallzrd@gmail.com>
2023-06-20 16:18:27 +0700

    init

---
+++ Scan2Cook/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Scan2Cook
+//
+//  Created by Haikal Lazuardi Fadil on 20/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scan2Cook/Scan2CookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scan2CookApp.swift
+//  Scan2Cook
+//
+//  Created by Haikal Lazuardi Fadil on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Scan2CookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
