bad2e47cf2c83d61bfa7435dfe5a960c5d13c86e
haikallf <haikallzrd@gmail.com>
2023-06-23 19:15:18 +0700

    add skeletons and refactor

---
+++ Scan2Cook/Views/BookmarkView.swift
@@ -0,0 +1,20 @@
+//
+//  BookmarkView.swift
+//  Scan2Cook
+//
+//  Created by Haikal Lazuardi Fadil on 23/06/23.
+//
+
+import SwiftUI
+
+struct BookmarkView: View {
+    var body: some View {
+        Text("This is Bookmark")
+    }
+}
+
+struct BookmarkView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookmarkView()
+    }
+}
---
+++ Scan2Cook/Views/Scan/ScanView.swift
@@ -0,0 +1,20 @@
+//
+//  ScanView.swift
+//  Scan2Cook
+//
+//  Created by Haikal Lazuardi Fadil on 23/06/23.
+//
+
+import SwiftUI
+
+struct ScanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScanView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScanView()
+    }
+}
