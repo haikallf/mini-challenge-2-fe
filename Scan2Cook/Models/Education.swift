37d3a51f7a93d9800b060535cca1a284426ab7d6
haikallf <haikallzrd@gmail.com>
2023-06-25 16:43:45 +0700

    add education view

---
+++ Scan2Cook/Models/Education.swift
@@ -0,0 +1,8 @@
+//
+//  Education.swift
+//  Scan2Cook
+//
+//  Created by Haikal Lazuardi on 25/06/23.
+//
+
+import Foundation
---
+++ Scan2Cook/Views/Scan/EducationView.swift
@@ -0,0 +1,20 @@
+//
+//  EducationView.swift
+//  Scan2Cook
+//
+//  Created by Haikal Lazuardi on 25/06/23.
+//
+
+import SwiftUI
+
+struct EducationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EducationView_Previews: PreviewProvider {
+    static var previews: some View {
+        EducationView()
+    }
+}
