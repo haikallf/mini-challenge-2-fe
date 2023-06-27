0057118c8a10c8fa14bc8d55b2d94c9fd0b634ab
Kevin Dallian <kevindallian@gmail.com>
2023-06-27 15:35:14 +0700

    added camera and model integration

---
+++ Scan2Cook/Core/MLModel/MLManager.swift
@@ -0,0 +1,75 @@
+//
+//  MLManager.swift
+//  Scan2Cook
+//
+//  Created by Kevin Dallian on 25/06/23.
+//
+
+import CoreML
+import SwiftUI
+import Vision
+class MLManager {
+    private var model : VNCoreMLModel? = nil
+    
+    init(){
+        self.model = loadModel()!
+        print("load model success")
+    }
+    
+    func loadModel() -> VNCoreMLModel? {
+        guard let modelURL = Bundle.main.url(forResource: "Scan2CookMLModel", withExtension: "mlmodelc") else {
+            print("Model file not found")
+            return nil
+        }
+        
+        guard let coreMLModel = try? MLModel(contentsOf: modelURL) else {
+            print("Failed to create MLModel")
+            return nil
+        }
+        
+        guard let visionModel = try? VNCoreMLModel(for: coreMLModel) else {
+            print("Failed to create VNCoreMLModel")
+            return nil
+        }
+        
+        return visionModel
+    }
+    
+    func detectObjects(in image: UIImage) -> [VNRecognizedObjectObservation]?{
+        var finalResults : [VNRecognizedObjectObservation] = [VNRecognizedObjectObservation]()
+        guard let ciImage = CIImage(image: image) else {
+            print("Failed to create CIImage from UIImage")
+            return nil
+        }
+        
+        let request = VNCoreMLRequest(model: model!) { request, error in
+            guard let results = request.results as? [VNRecognizedObjectObservation] else {
+                print("Failed to process the results")
+                return
+            }
+            if results.isEmpty {
+                print("Result empty")
+            }else {
+                for result in results {
+//                    let boundingBox = result.boundingBox
+//                    let confidence = result.confidence
+                    let resultsFiltered = result.labels.filter { label in
+                        label.confidence >= 0.8
+                    }
+                    for className in resultsFiltered{
+                        print("Detected \(className.identifier), with confidence \(className.confidence)")
+                    }
+                }
+            }
+            finalResults = results
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: ciImage)
+        do {
+            try handler.perform([request])
+        } catch {
+            print("Error performing object detection: \(error.localizedDescription)")
+        }
+        return finalResults
+    }
+}
